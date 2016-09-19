6adb55f8e921215af64df8100f56f3b784f3c9b0
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-09-19 16:19:39 +0100

    can search for users

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("hey");
+var express = require("express");
+var app = express();
+
+var myPort = 3000 || process.env.PORT;
+
+var users = [
+  {"name" : "Davidino",
+  "age" : 43},
+  {"name" : "CaptainCam",
+  "age" : 22},
+  {"name" : "Razki",
+  "age" : 69}
+]
+
+//routers
+app.get("/", function(req, res, next) {
+  res.send("default users brav");
+});
+
+app.get("/users", function(req, res, next) {
+  res.send(users);
+});
+
+app.get("/users/:id", function(req, res, next) {
+  res.send(users[req.params.id]);
+});
+
+
+
+
+//begin server
+app.listen(myPort, function() {
+  console.log(`Your app is ready and listening on port ${myPort}`);
+});
